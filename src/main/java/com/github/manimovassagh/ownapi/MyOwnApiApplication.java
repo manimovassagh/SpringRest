e7796a264a386710824eec7b5fa6a383bf622ee3
manimovassagh <mani.mgh@gmail.com>
2021-08-27 13:58:40 +0200

    1

---
+++ src/main/java/com/github/manimovassagh/ownapi/MyOwnApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.manimovassagh.ownapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/manimovassagh/ownapi/model/Staff.java
@@ -0,0 +1,66 @@
+package com.github.manimovassagh.ownapi.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "staff")
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "staff_id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email_id;
+
+    public Staff() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email_id='" + email_id + '\'' +
+                '}';
+    }
+}

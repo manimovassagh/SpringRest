d5edb1e5894f591c8c673d22e556dcc6165c6b76
Mani Movassagh <mani.mgh@gmail.com>
2021-12-19 16:04:18 +0100

    11!

--- src/main/java/com/github/manimovassagh/ownapi/controller/StaffController.java
+++ src/main/java/com/github/manimovassagh/ownapi/controller/StaffController.java
@@ -42,7 +42,6 @@ public class StaffController {
 
 
 
-    @JsonFormat
     @RequestMapping()
     @PutMapping("/staffs/{id}")
     public ResponseEntity<Staff> updateEmployee(@PathVariable Long id, @RequestBody Staff employeeDetails) {
